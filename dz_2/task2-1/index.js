751fc305139942157397c98e54450bdc89467865
Innokentii Finkel <keshafinkel@gmail.com>
2015-06-08 21:41:54 +0300

    fisrt

--- dz_2/task2-1/index.js
+++ dz_2/task2-1/index.js
@@ -1,24 +1,23 @@
 var tasks = [];
 /*add task function*/
 function add_task(event){
-    if (document.querySelector("input[type=text]").value) {
-        var task_text = document.querySelector("input[type=text]").value;
-
+    if (document.querySelector('input[type=text]').value) {
+        var task_text = document.querySelector('input[type=text]').value;
         render_task(task_text);
         tasks.push(task_text);
         tasks.sort();
         window.localStorage.setItem('tasks',JSON.stringify(tasks));
-        document.querySelector("input[type=text]").value = "";
+        document.querySelector('input[type=text]').value = '';
     }
 }
 /*append task to ul with tasks*/
 function render_task(text) {
-    var task = document.createElement("li");
+    var task = document.createElement('li');
 
-    task.innerHTML = text;
-    document.querySelector("ul").appendChild(task);
+    task.textContent = text;
+    document.querySelector('ul').appendChild(task);
 }
-/*call addtask on "enter"*/
+/*call addtask on 'enter'*/
 function add_task_press(event){
     if (event.keyCode == 13) {
         add_task();
@@ -27,18 +26,16 @@ function add_task_press(event){
 
 /* get function*/
 function get_tasks(){
-    if (localStorage.tasks) {
-        tasks = JSON.parse(localStorage.tasks);
+    if (localStorage.getItem('tasks')) {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
         for (var i=0;i < tasks.length; i++) {
             render_task(tasks[i]);
         }
-    } else {
-        console.log("asd");
     }
 }
 /* init function*/
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
     get_tasks();
-    document.querySelector("button").addEventListener("click",add_task);
-    document.querySelector("input").addEventListener("keypress",add_task_press);
+    document.querySelector('button').addEventListener('click',add_task);
+    document.querySelector('input').addEventListener('keypress',add_task_press);
 });
--- dz_2/task2-2/index.js
+++ dz_2/task2-2/index.js
@@ -1,48 +1,53 @@
-var input_1 = document.createElement("input"),
-    input_2 = document.createElement("input"),
-    button = document.createElement("button"),
-    result = document.createElement("div");
-result.id = "result";
-button.innerHTML = "Посчитать";
-button.addEventListener("click",validate);
-
-/*render html*/
-function renter_html() {
-    document.querySelector("body").appendChild(input_1);
-    document.querySelector("body").appendChild(input_2);
-    document.querySelector("body").appendChild(button);
-}
-/*add error function*/
-function error(element) {
-    var check = (/(^[\d]+[.]?[\d]+$)|(^\d+$)/);
-    if(!(check.test(element.value))) {
-        if (!(element.nextSibling.classList.contains("error-message"))) {
-            var error = document.createElement("div");
-            error.className = "error-message";
-            error.innerHTML = "Это не число";
-            element.parentNode.insertBefore(error, element.nextSibling);
-        }
-        return false;
-    } else {
-        if (element.nextSibling.classList.contains("error-message")) {
-            element.parentNode.removeChild(element.nextSibling);
-        }
-        return true;
-    }
-}
-/*validate function*/
-function validate(){
-    result.innerHTML = "";
-    if(error(input_1) && error(input_2)){
-        result.innerHTML = Number(input_1.value) + Number(input_2.value);
-        document.querySelector("body").appendChild(result);
-    }
-}
-
 /* init function*/
-function init(){
-    renter_html();
-}
-document.addEventListener("DOMContentLoaded", function(event) {
-    init();
+window.addEventListener('load', function() {
+    'use strict';
+
+    var input1 = document.createElement('input');
+    var input2 = document.createElement('input');
+    var button = document.createElement('button');
+    var result = document.createElement('div');
+
+    /*render html*/
+    function renterHtml() {
+        document.querySelector('body').appendChild(input1);
+        document.querySelector('body').appendChild(input2);
+        document.querySelector('body').appendChild(button);
+    }
+    /*add error function*/
+    function error(element) {
+        var check = (/(^[\d]+[.]?[\d]+$)|(^\d+$)/);
+        if(!(check.test(element.value))) {
+            if (!(element.nextSibling.classList.contains('error-message'))) {
+                var error = document.createElement('div');
+                error.className = 'error-message';
+                error.innerHTML = 'Это не число';
+                element.parentNode.insertBefore(error, element.nextSibling);
+            }
+            return false;
+        } else {
+            if (element.nextSibling.classList.contains('error-message')) {
+                element.parentNode.removeChild(element.nextSibling);
+            }
+            return true;
+        }
+    }
+    /*validate function*/
+    function validate(){
+        var flag_1 = error(input1),
+            flag_2 = error(input2);
+        if (document.querySelector('#result')){
+            result.parentNode.removeChild(result);
+        }
+
+        if (flag_1 && flag_2) {
+            result.innerHTML = Number(input1.value) + Number(input2.value);
+            document.querySelector('body').appendChild(result);
+        }
+    }
+
+    result.id = 'result';
+    button.innerHTML = 'Посчитать';
+    button.addEventListener('click', validate);
+
+    renterHtml();
 });
