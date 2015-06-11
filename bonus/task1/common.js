8664819d6389bebc0814436e4ab34a25d5dc2071
KeshaFinkel <keshafinkel@gmail.com>
2015-06-11 18:08:22 +0300

    dz3_1

--- bonus/task1/common.js
+++ bonus/task1/common.js
@@ -1,6 +1,26 @@
 /**
  * Created by user on 11.06.2015.
  */
-function handleClickOutside(element, fn){
 
+/* dat funct*/
+function handleClickOutside(element, fn){
+  element.addEventListener("click", function(){
+    if(event.target != element) {
+
+    }
+    console.log(event);
+  });
 }
+
+/**/
+function draw(element) {
+  element.style.background = "rgba(90,0,0,0.3)";
+}
+
+/* init function*/
+document.addEventListener('DOMContentLoaded', function(event) {
+  var divs = document.querySelectorAll("div");
+  for(var i = 0; i < divs.length;i++){
+    handleClickOutside(divs[i], draw);
+  }
+});
