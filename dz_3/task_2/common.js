443c3571a62729e83c2706082acf86b8dbf2570c
Innokentii Finkel <keshafinkel@gmail.com>
2015-06-14 21:16:59 +0300

    3.3start

--- dz_3/task_2/common.js
+++ dz_3/task_2/common.js
@@ -3,41 +3,42 @@
  */
 
 /*send request*/
-function send(type,element) {
-    var req = new XMLHttpRequest();
-    req.open(type , 'https://cors-test.appspot.com/test', true);
-    req.addEventListener('readystatechange', function(e){
-        if ((req.readyState == 4) && (req.status == 200)) {
-            console.log(JSON.parse(req.response));
-            if (JSON.parse(req.response).status == 'ok'){
-                element.textContent = 'OK';
-                element.style.color = 'green';
-                element.style.fontWeight = 'bold';
-            }else {
-                element.textContent = 'Failed';
-                element.style.color = 'red';
-                element.style.fontWeight = 'bold';
-            }
+function send(type, element) {
+  var req = new XMLHttpRequest();
+  req.open(type, 'https://cors-test.appspot.com/test', true);
+  req.addEventListener('readystatechange', function (e) {
+    console.log(req);
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        if (JSON.parse(req.response).status == 'ok') {
+          element.textContent = 'OK';
+          element.style.color = 'green';
+          element.style.fontWeight = 'bold';
+        } else {
+          element.textContent = 'Failed';
+          element.style.color = 'red';
+          element.style.fontWeight = 'bold';
         }
-    });
-    req.addEventListener("error", function(e){
+      } else {
         element.textContent = 'Failed';
         element.style.color = 'red';
         element.style.fontWeight = 'bold';
-    });
-    req.send(null);
+      }
+    }
+  });
+  req.send(null);
 }
 
 
 /* render*/
-function render(element){
-    var type = element.className;
-    send(type,element);
+function render(element) {
+  var type = element.className;
+  send(type, element);
 }
 /*init*/
 window.addEventListener('load', function () {
-    var spans = document.querySelectorAll('span');
-    for(var i=0; i<spans.length; i++){
-        render(spans[i]);
-    }
+  var spans = document.querySelectorAll('span');
+  for (var i = 0; i < spans.length; i++) {
+    render(spans[i]);
+  }
 });
